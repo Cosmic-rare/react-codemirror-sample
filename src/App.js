b03149b35c008b291e1c7293c3b179edc25d5dc8
Cosmic-rare <ysolakey1116@gmail.com>
2021-11-29 20:18:28 +0900

    use localstorage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
+/** @jsxImportSource @emotion/react */
+
 import CodeMirror from '@uiw/react-codemirror';
 import { markdown, markdownLanguage } from '@codemirror/lang-markdown'
 import { languages } from '@codemirror/language-data'
 import { oneDark } from '@codemirror/theme-one-dark'
 import { defaultHighlightStyle, HighlightStyle, tags } from '@codemirror/highlight'
 import { EditorView } from '@codemirror/view'
+import { useState } from "react"
+import { css } from "@emotion/react"
 
 import "./index.css"
+
+const buttonStyle = css`
+  background-color: #2e2e2e;
+  color: azure;
+`
 
 const transparentTheme = EditorView.theme({
   '&': {
@@ -32,11 +41,21 @@ const syntaxHighlighting = HighlightStyle.define([
   }
 ])
 
-export default function App() {
+const App = () => {
+  const [Markdown, setMarkdown] = useState("")
+  
+  const onChange = (value, valueUpdate) => {
+    setMarkdown(value)
+  }
   return (
     <div className="editor">
+
+      <button onClick={() => {
+        localStorage.setItem('markdown', Markdown)
+      }} css={buttonStyle}>Save</button>
+
       <CodeMirror
-        value="# Hello, World!"
+        value={localStorage.getItem("markdown") || "# Hello, World!"}
         extensions={[
           markdown({
             base: markdownLanguage,
@@ -49,11 +68,10 @@ export default function App() {
           transparentTheme,
           EditorView.lineWrapping
         ]}
-        onChange={(value, viewUpdate) => {
-          console.log('value:', value);
-          console.log(viewUpdate)
-        }}
+        onChange={onChange}
       />
     </div>
   );
 }
+
+export default App
